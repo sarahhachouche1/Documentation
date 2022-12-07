b2b326517c8166da610de05445276e0b2945e33f
sarah hachouche <sarahhachouche7@gmail.com>
2022-12-07 21:36:33 +0000

    7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step 7/step7.js
@@ -0,0 +1,11 @@
+function multipleOperations()
+{
+  var shoe_size, birth_year, res;
+  shoe_size= parseInt(document.getElementById("shoe_size").value);
+   birth_year = parseInt(document.getElementById("year").value);
+   res=(shoe_size*2+5)*50-birth_year+1766;
+   console.log(res);
+  document.getElementById("txtresult").value = res;
+
+  
+} 
