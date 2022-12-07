0064b79293f5ff4184a151aeec46a771906f39c2
sarah hachouche <sarahhachouche7@gmail.com>
2022-12-07 23:55:27 +0000

    step1

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/step1.js
@@ -0,0 +1,7 @@
+var element = document.querySelector('#image1');
+element.onmouseover=function(){
+    element.src="images/image1_2.jpg";
+}
+element.onmouseout=function(){
+    element.src="images/image1.jpg";
+}
