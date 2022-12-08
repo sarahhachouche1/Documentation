02b296ac8f6537606d23914e1ddbd68c466b6917
sarah hachouche <sarahhachouche7@gmail.com>
2022-12-08 16:20:21 +0000

    step2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/step2.js
@@ -0,0 +1,12 @@
+function fn(a)
+{
+    
+    if(a==1)
+    {
+       document.getElementById("texte").style.display="block";
+    }
+    else{
+        document.getElementById("texte").style.display="none";
+    }
+     
+}
