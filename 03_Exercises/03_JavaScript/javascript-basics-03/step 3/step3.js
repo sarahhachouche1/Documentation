5be8932ee08dec377ecc85dd09c1e28e00eaa1d7
sarah hachouche <sarahhachouche7@gmail.com>
2022-12-08 16:50:56 +0000

    step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/step3.js
@@ -0,0 +1,16 @@
+function fn(a)
+{
+    if(a==1)
+    {
+        document.getElementById("text").style.color="green"
+    }
+    else if(a==2)
+    {
+        document.getElementById("text").style.color="red"
+
+    }
+    else if(a==3){
+        document.getElementById("text").style.color="blue"
+
+    }
+}
