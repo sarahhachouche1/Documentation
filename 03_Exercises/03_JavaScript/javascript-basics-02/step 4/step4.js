e0edb14593de09fe07cbcf59145fc45715bed404
sarah hachouche <sarahhachouche7@gmail.com>
2022-12-08 10:56:45 +0000

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step4.js
@@ -0,0 +1,10 @@
+function fh(){
+    let res=prompt("you want to rest");
+    if(res=="yes")
+    {
+        document.getElementById('name').value = '';
+        document.getElementById('surname').value = '';
+        document.getElementById('city').value = '';
+    }
+
+}
