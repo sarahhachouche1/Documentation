6b241897de2c8d8ffd4052d5b12ec724ad3d8226
sarah hachouche <sarahhachouche7@gmail.com>
2022-12-09 20:25:18 +0000

    2

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -24,3 +24,25 @@ WHERE Name = 'Basma';
 UPDATE students
 SET Points = '100'
 WHERE Name = 'Alex';   
+----CREATE TABLE---
+CREATE TABLE GRADUATES(
+   ID INT AUTO_INCREMENT,
+   NAME TEXT NOT NULL UNIQUE,
+   AGE  INT,
+   Gender TEXT,
+   Points INT,   
+   Graduations TEXT,
+   PRIMARY KEY (ID)
+);
+10-
+INSERT INTO GRADUATES (ID, NAME, AGE,Gender,Points)
+SELECT ID, Name,Age,Gender,Points
+FROM students
+Where Name='Layal'
+11-
+UPDATE GRADUATES
+SET Graduations='08/09/2018'
+WHERE Name='Layal'
+12-
+DELETE FROM students
+WHERE Name='Layal';
